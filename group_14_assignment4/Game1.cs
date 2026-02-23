24dbb0dfa15cf0198282f5909e467f88c4f2d9a9
meagan8 <hennigmt@gmail.com>
2026-02-22 19:41:58 -0600

    added rocket class

--- group_14_assignment4/Game1.cs
+++ group_14_assignment4/Game1.cs
@@ -9,18 +9,34 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-
+    
+    // fields for meteor  
     private Meteor meteor;
     private Meteor comet;
     
     private Vector2 meteorStartPosition;
     private Vector2 cometStartPosition;
     
+    // fields for planet  
+    private Texture2D _planet; 
+    private Vector2 _planetPosition;
+    
+    // fields for rocket  
+    private Texture2D _rocket1Texture;
+    private Texture2D _1exhaustTexture;
+    private Rocket _smallerRocket;
+    
+    private Texture2D _rocket2Texture;
+    private Texture2D _2exhaustTexture;
+    private Rocket _LargerRocket2;
+    
+    // fields for star
     private Texture2D _starTexture;
     private Texture2D _sparkleTexture;
 
     private List<Star> _stars;
 
+    // screen size info
     private int _screenWidth;
     private int _screenHeight;
 
@@ -40,6 +56,10 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        // initialization for planet position 
+        _planetPosition = new Vector2(-250, 500);
+        
+        // initialization for stars 
         _stars = new List<Star>();
 
         base.Initialize();
@@ -47,9 +67,9 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        
-        meteorStartPosition = new Vector2(800, 200);
-        cometStartPosition = new Vector2(800, 400);
+        // loading content for meteor
+        meteorStartPosition = new Vector2(1000, 50);
+        cometStartPosition = new Vector2(850, 150);
         
         meteor = new Meteor(Content.Load<Texture2D>("imgs/meteor_body"), 
             Content.Load<Texture2D>("imgs/meteor_tail")
@@ -58,8 +78,46 @@ public class Game1 : Game
             Content.Load<Texture2D>("imgs/comet_tail")
             ,2f, cometStartPosition);
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        // loading content for planet 
+        _planet = Content.Load<Texture2D>("imgs/planet");
+        
+        // loading content for rocket
+        _rocket1Texture = Content.Load<Texture2D>("imgs/rocket1");
+        _rocket2Texture = Content.Load<Texture2D>("imgs/rocket2");
+        
+        // load content for exhust 
+        _1exhaustTexture = Content.Load<Texture2D>("imgs/fire1");
+        _2exhaustTexture = Content.Load<Texture2D>("imgs/fire2");
+
+        _smallerRocket = new Rocket(_rocket1Texture, 
+            _1exhaustTexture, 
+            new Vector2(600, 1200),
+            new Vector2(0, 150), 
+            1.0f, 
+            0.99f, 
+            1.0f, 
+            0.09f,
+            7.0f, 
+            20.0f,
+            -30.0f);
+        
+        //rocket2
+        _LargerRocket2 = new Rocket(_rocket2Texture,
+            _2exhaustTexture,
+            new Vector2(50, 1000),
+            new Vector2(0, 100),
+            1.0f,
+            0.99f,
+            1.0f,
+            0.09f,
+            25.0f,
+            20.0f,
+            -70.0f);
+
 
         // TODO: use this.Content to load your game content here
+        // loading content for stars 
         _starTexture = Content.Load<Texture2D>("imgs/star");
         _sparkleTexture = Content.Load<Texture2D>("imgs/sparkle");
 
@@ -78,14 +136,21 @@ public class Game1 : Game
 
         // TODO: Add your update logic here
         
-        
+        // update for meteor
         meteor.Animate(gameTime, meteorStartPosition, new Vector2(0, 300), 5);
         comet.Animate(gameTime, cometStartPosition, new Vector2(0, 500), 5);
         
+        // update for rocket 
+        _smallerRocket.Update(gameTime);
+        _LargerRocket2.Update(gameTime);
+        
+        
+        // update for stars
         foreach (var star in _stars)
         {
             star.Update(gameTime);
         }
+        
         
         base.Update(gameTime);
     }
@@ -96,6 +161,7 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         
+        // drawing for stars 
         foreach (var star in _stars)
         {
             _spriteBatch.Begin(transformMatrix: star.WorldMatrix);
@@ -103,8 +169,31 @@ public class Game1 : Game
             _spriteBatch.End();
         }
         
+        _smallerRocket.Draw(_spriteBatch);
+
+        
+        // drawing for meteor 
         meteor.Draw(_spriteBatch);
         comet.Draw(_spriteBatch);
+    
+        
+        //drawing planet 
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            _planet,
+            _planetPosition,
+            sourceRectangle: null,
+            Color.White,
+            0.0f,
+            Vector2.Zero,
+            scale: 2.0f,
+            SpriteEffects.None,
+            layerDepth: 0.0f
+        );
+        _spriteBatch.End();
+        
+        // drawing rocket 
+        _LargerRocket2.Draw(_spriteBatch);
         
        
 
---
+++ group_14_assignment4/Rocket.cs
@@ -0,0 +1,191 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace group_14_assignment4;
+
+public class Rocket
+{
+    // fields 
+    // textures 
+    private Texture2D _rocketBody;
+    
+    // movement 
+    private Vector2 _startRocketPosition;
+    private Vector2 _initialRocketPosition;
+    private Vector2 _rocketSpeed;
+    
+    // scaling
+    private float _maxRocketScale;      
+    private float _minRocketScale;  
+    private float _scaleSpeed; 
+    private float _currentRocketScale;
+    private bool _isScalingDown = true;
+
+    // exhaust
+    private Exhaust _exhaust;  
+    private float _exhaustShakeAmount;
+    private float _exhaustShakeSpeed;
+    private float _exhaustVerticalOffset;
+    
+    // transformation
+    private Matrix _rocketTransformation = Matrix.Identity;
+    
+    // constructor 
+    public Rocket(Texture2D rocketBody, 
+        Texture2D exhaustTexture, 
+        Vector2 startRocketPosition, 
+        Vector2 rocketSpeed,
+        float maxRocketScale, 
+        float minRocketScale, 
+        float initialRocketScale, 
+        float scaleSpeed, 
+        float exhaustShakeAmount, 
+        float exhaustShakeSpeed,
+        float exhaustVerticalOffset)
+    {
+        _rocketBody = rocketBody;
+        _startRocketPosition = startRocketPosition;
+        _initialRocketPosition = startRocketPosition;
+        _rocketSpeed = rocketSpeed;
+
+        _maxRocketScale = maxRocketScale;
+        _minRocketScale = minRocketScale;
+        _currentRocketScale = initialRocketScale;
+        _scaleSpeed = scaleSpeed;
+        
+        _exhaustVerticalOffset = exhaustVerticalOffset;
+
+        //_exhaustShakeAmount = exhaustShakeAmount;
+        //_exhaustShakeSpeed = exhaustShakeSpeed;
+    
+        _exhaust = new Exhaust(exhaustTexture, exhaustShakeAmount, exhaustShakeSpeed, _exhaustVerticalOffset);
+    }
+    
+    // overall movement 
+    public void Update(GameTime gameTime)
+    {
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        // move rocket
+        _startRocketPosition.Y -= _rocketSpeed.Y * dt;
+        
+        if (_isScalingDown)
+        {
+            _currentRocketScale -= _scaleSpeed * dt;
+            //_currentRocketScale -= 0.01f;
+
+            // if it hits the min scale make _isScalingDown false
+            if (_currentRocketScale <= _minRocketScale)
+            {
+                _currentRocketScale = _minRocketScale;
+                _isScalingDown = false;
+            }
+        }
+        else
+        {
+            _currentRocketScale += _scaleSpeed * dt;
+            //_currentRocketScale += 0.01f;
+            
+            // changes _isScalingDown to true when it hits max
+            if (_currentRocketScale >= _maxRocketScale)
+            {
+                _currentRocketScale = _maxRocketScale;
+                _isScalingDown = true;
+            }
+        }
+        
+        // creating rocket transform matrix 
+        _rocketTransformation =
+            Matrix.CreateScale(_currentRocketScale) *
+            Matrix.CreateTranslation(new Vector3(_startRocketPosition, 1));
+        
+        // exhaust
+        Vector2 rocketSize = new Vector2(_rocketBody.Width, _rocketBody.Height);
+        _exhaust.SetOffSetFromRocket(rocketSize);
+        
+        // resetting
+        if (IsAboveScreen())
+        {
+            Reset();
+        }
+        
+        _exhaust.Update(gameTime);
+}
+
+    public void Reset()
+    {
+        _startRocketPosition = _initialRocketPosition;
+        _currentRocketScale = _maxRocketScale;
+        _isScalingDown = true;
+    }
+
+    public bool IsAboveScreen()
+    {
+        return _startRocketPosition.Y + (_rocketBody.Height * _currentRocketScale) < 0;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        // drawing exhaust (drawing this first so it will appear on bottom)
+        _exhaust.Draw(spriteBatch, _rocketTransformation);
+        
+        // drawing rocket
+        spriteBatch.Begin(transformMatrix: _rocketTransformation);
+        spriteBatch.Draw(_rocketBody, Vector2.Zero, Color.White);
+        spriteBatch.End();
+    }
+}
+
+public class Exhaust
+{
+    // texture 
+    private Texture2D _exhaustTexture;
+
+    // animation settings 
+    private float _shakeAmount;     // how far it moves vertically
+    private float _shakeSpeed;      // how fast is oscillates
+    
+    private float _time;        // tracks sin wave phase 
+    private Vector2 _offSetFromRocket;
+    private Matrix _exhaustTransform = Matrix.Identity;
+    private float _verticalOffset;
+    
+
+    public Exhaust(Texture2D exhaustTexture, float shakeAmount, float shakeSpeed, float verticalOffset)
+    {
+        _exhaustTexture = exhaustTexture;
+        _shakeAmount = shakeAmount;
+        _shakeSpeed = shakeSpeed;
+        _offSetFromRocket = Vector2.Zero;
+        _verticalOffset = verticalOffset; 
+    }
+    
+    public void SetOffSetFromRocket(Vector2 rocketSize)
+    {
+        _offSetFromRocket = new Vector2((rocketSize.X / 2f) , rocketSize.Y + _verticalOffset);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        _time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        // sine wave shake
+        float yOffset = (float)Math.Sin(_time * _shakeSpeed) * _shakeAmount;
+
+        _exhaustTransform =
+            Matrix.CreateTranslation(new Vector3(_offSetFromRocket.X - (_exhaustTexture.Width / 2f), 
+                _offSetFromRocket.Y + yOffset, 0));
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Matrix parentTransform)
+    {
+        Matrix world =
+            _exhaustTransform *
+            parentTransform;
+
+        spriteBatch.Begin(transformMatrix: world);
+        spriteBatch.Draw(_exhaustTexture, Vector2.Zero, Color.White);
+        spriteBatch.End();
+    }
+}
